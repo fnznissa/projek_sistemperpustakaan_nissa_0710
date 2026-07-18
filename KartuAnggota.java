92fb3516071ee4c04f84eb8393711a1ad46be25f
Farisha Na'ila Zahronissa <farisha.zahronissa25@student.uisi.ac.id>
2026-07-18 15:38:31 +0700

    Update KartuAnggota.java

--- KartuAnggota.java
+++ KartuAnggota.java
@@ -1,24 +1,20 @@
 import java.time.LocalDate;
 
 public class KartuAnggota {
-
-    private final String nomorKartu; //enkapsulasi
+    // PILAR OOP: ENCAPSULATION (Menggunakan private final agar datanya tidak bisa diubah setelah dibuat)
+    private final String nomorKartu;
     private final LocalDate tanggalDaftar;
 
     public KartuAnggota(String nomorKartu) {
         this.nomorKartu = nomorKartu;
-        this.tanggalDaftar = LocalDate.now();
+        this.tanggalDaftar = LocalDate.now(); // Otomatis mencatat waktu saat ini
     }
 
-    public String getNomorKartu() {
-        return nomorKartu;
-    }
+    public String getNomorKartu() { return nomorKartu; }
+    public LocalDate getTanggalDaftar() { return tanggalDaftar; }
 
-    public LocalDate getTanggalDaftar() {
-        return tanggalDaftar;
-    }
-
-    @Override //abstraksi
+    //polimorfisme
+    @Override
     public String toString() {
         return "Kartu No." + nomorKartu + " (terdaftar " + tanggalDaftar + ")";
     }
