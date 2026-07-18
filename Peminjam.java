6ba2513a40ad7290c6f24d46f0ea18b25beb3ebc
Farisha Na'ila Zahronissa <farisha.zahronissa25@student.uisi.ac.id>
2026-07-18 15:40:19 +0700

    Update Peminjam.java

--- Peminjam.java
+++ Peminjam.java
@@ -1,8 +1,10 @@
-
+// PILAR OOP: ABSTRACTION (Abstraksi Penuh lewat Interface)
+// KONSEP: Interface (Kontrak kelakuan yang wajib dimiliki objek peminjam)
+// =========================================================================
 public interface Peminjam {
 
-    void pinjamBuku(Buku buku);
+    void pinjamBuku(Buku buku); // Abstract method (implisit)
 
     // hariTerlambat = jumlah hari melewati batas maksimal peminjaman
-    double hitungDenda(long hariTerlambat);
+    double hitungDenda(long hariTerlambat); // Abstract method
 }
